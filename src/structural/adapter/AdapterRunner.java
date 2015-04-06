77af54c9059accccfd64c239cee15095b6d0149e
Volodymyr Monchak <vov4enso@gmail.com>
2015-04-06 15:17:07 +0300

    Adapter implemented

--- src/structural/adapter/AdapterRunner.java
+++ src/structural/adapter/AdapterRunner.java
@@ -2,7 +2,9 @@ package structural.adapter;
 
 public class AdapterRunner {
 
-	
+	/**
+	 * http://www.avajava.com/tutorials/lessons/adapter-pattern.html?page=2
+	 */
 	public static void main(String[] args) {
 		Adapter adapter = new Adapter();
 		adapter.setTemp(120);
